e09eeb415723626b822a8a8c4fffc69a5666f9e1
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-18 18:17:23 +0000

    return and assert tail of a given array

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+let copyArray;
+let tailArray = [];
+
+const tail = fullArray => {
+  copyArray = fullArray;
+  for (let i = 1; i < fullArray.length; i++) {
+    tailArray.push(fullArray[i]);
+  }
+  return tailArray;
+};
+
+tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(tailArray.length, copyArray.length - 1);
+
+for (let j = 1; j < copyArray.length; j++) {
+  let k = j - 1;
+  assertEqual(copyArray[j], tailArray[k]);
+}
