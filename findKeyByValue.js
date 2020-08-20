94121458b50aa11137f0feb0b7989e7b2b314ea4
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-20 07:17:41 +0000

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(givenObj, givenValue) {
+  for (let objKey in givenObj) {
+    if (givenObj[objKey] === givenValue) {
+      return objKey;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre,"The Wire"));
+console.log(findKeyByValue(bestTVShowsByGenre,"Lion King"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Taken"), "broken");
